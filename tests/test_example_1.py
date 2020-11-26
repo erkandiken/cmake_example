5e09e55cc2f26f1455ca9921990b1756bd8fd207
Erkan_mac16 <erkandiken@gmail.com>
2020-11-26 14:37:00 +0100

    fix assertion error

--- tests/test_example_1.py
+++ tests/test_example_1.py
@@ -2,7 +2,7 @@ import cpp_examples as m
 
 A = [1., 2., 3., 4.]
 result = m.modify(A)
-expected_result = [2., 4., 6., 10.]
+expected_result = [2., 4., 6., 8.]
 
 try:
     assert result == expected_result, "assertion error"
