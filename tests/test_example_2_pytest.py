d180040ed05e87f0a4aacf1a3a7a4669409f2072
Erkan_mac16 <erkandiken@gmail.com>
2020-11-30 21:14:35 +0100

    minor changes in pytest files

--- tests/test_example_2_pytest.py
+++ tests/test_example_2_pytest.py
@@ -6,7 +6,7 @@ class TestExample2(unittest.TestCase):
     def test_modify_nested_list(self):
         A = [[1,2,3,4],[5,6]]
         result = m.modify_nested_list(A)
-        expected_result = [[2,4,6,8],[10,12]]
+        expected_result = [[2,4,6,8], [10,12]]
         self.assertEqual(result, expected_result, "assertion error")
 
 if __name__ == '__main__':
--- tests/test_example_3_pytest.py
+++ tests/test_example_3_pytest.py
@@ -7,14 +7,14 @@ class TestExample3(unittest.TestCase):
     def test_multiply(self):
         A = [0,1,2,3,4,5]
         result = m.multiply(A)
-        expected_result = np.arange(0, 60, 10).tolist()
-        self.assertEqual(result.tolist(), expected_result, "assertion error")
+        expected_result = np.arange(0, 60, 10)
+        self.assertSequenceEqual(result.tolist(), expected_result.tolist(), "assertion error")
 
     def test_multiply_with_numpy(self):
         A = np.arange(10)
         result = m.multiply(A)
         expected_result = np.arange(0, 100, 10)
-        self.assertEqual(result.all(), expected_result.all(), "assertion error")
+        self.assertSequenceEqual(result.tolist(), expected_result.tolist(), "assertion error")
 
 if __name__ == '__main__':
     unittest.main()
